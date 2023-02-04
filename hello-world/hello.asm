627ae0e103590d45be2c0cad708639453f3e8b87
mdmassa <djsheepwolfbr@gmail.com>
2023-02-04 13:56:57 -0300

    Edited hello.asm

--- hello-world/hello.asm
+++ hello-world/hello.asm
@@ -1,20 +1,22 @@
+; Linguagem Assembly
+; Exercício 01
+
+; Programa "Hello World!"
+
 section .data
-    msg db 'Hello World', 0xA
-    tam equ $ - msg    
+    msg db 'Hello World!', 0xA  ; Mensagem que será enviada na saída
+    len equ $ - msg             ; Definindo o tamanho da mensagem
 
 section .text
+    global _start
+    _start:
+        mov eax, 0x4            ; Definindo que o programa requer uma saída
+        mov ebx, 0x1            ; Definindo que a mensagem será enviada na saída padrão (Ou seja, o terminal)
+        mov ecx, msg            ; Conteúdo da saída
+        mov edx, len            ; Quantidade de caracteres da saída
+        int 0x80                ; Comunicação com o SO para a execução dos comandos
 
-
-global _start
-
-
-_start:
-    mov eax, 0x4
-    mov ebx, 0x1
-    mov ecx, msg
-    mov edx, tam
-    int 0x80
-
-    mov eax, 0x1
-    mov ebx, 0x0
-    int 0x80
+    finish:
+        mov eax, 0x1            ; Informa a finalização do programa
+        mov ebx, 0x0            ; Retorna o valor 0 para indicar que tudo ocorreu corretamente
+        int 0x80                ; Comunicação com o SO para a execução dos comandos
