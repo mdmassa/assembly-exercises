ca82d2f088703f7b32059dcb6d39273da905cf17
mdmassa <djsheepwolfbr@gmail.com>
2023-02-07 08:29:44 -0300

    Added comments

--- library/testLibrary.asm
+++ library/testLibrary.asm
@@ -1,3 +1,8 @@
+; Linguagem Assembly
+; Exercício 04
+
+; Programa "Criando uma biblioteca e trabalhando com Buffers"
+
 %include 'library.inc'
 
 section .text
@@ -9,11 +14,11 @@ section .text
         add esi, 0x9        ; Vai adicionar o início de uma string a ESI
         mov byte[esi], 0xA  ; Adicionando uma quebra de linha ao registrador ESI
 
-        dec esi
-        mov dl, 0x41
-        mov [esi], dl
+        dec esi             ; Decrementar 1 de ESI
+        mov dl, 0x41        ; Movendo o caractere A para o registrador DL
+        mov [esi], dl       ; Movendo valor no DL para o registrador ESI
 
-        call outputRes
+        call outputRes      ; Chamando outputRes da biblioteca library.inc
 
         mov eax, 0x1
         xor ebx, ebx
